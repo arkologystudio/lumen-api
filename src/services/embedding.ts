afa1b7df66c35dad4b1142fda4d86b73ea7122da
CheCoelho <checoelho@gmail.com>
2025-05-06 21:02:06 +0200

    minor changes

--- src/services/embedding.ts
+++ src/services/embedding.ts
@@ -1,5 +1,9 @@
 import { ENV } from "../config/env";
-import { HfInference, FeatureExtractionOutput } from "@huggingface/inference";
+
+import {
+  InferenceClient,
+  FeatureExtractionOutput,
+} from "@huggingface/inference";
 
 export const embedText = async (
   userQuery: string
@@ -9,15 +13,27 @@ export const embedText = async (
     console.log("Hugging Face API token:", ENV.HUGGING_FACE_API_TOKEN);
     console.log("Embedding model:", ENV.EMBEDDING_MODEL);
     console.log("Inference provider:", ENV.INFERENCE_PROVIDER);
-    const client = new HfInference(ENV.HUGGING_FACE_API_TOKEN);
+    // const client = new HfInference(ENV.HUGGING_FACE_API_TOKEN);
 
-    const output = await client.featureExtraction({
-      model: ENV.EMBEDDING_MODEL,
+    // const output = await client.featureExtraction({
+    //   model: ENV.EMBEDDING_MODEL,
+    //   inputs: userQuery,
+    //   provider: ENV.INFERENCE_PROVIDER,
+    // });
+
+    // return output;
+    if (!ENV.HUGGING_FACE_API_TOKEN) {
+      throw new Error("HUGGING_FACE_API_TOKEN is not defined");
+    }
+    const client = new InferenceClient(ENV.HUGGING_FACE_API_TOKEN);
+    console.log("Client created");
+    const embeddingResult = await client.featureExtraction({
+      model: ENV.EMBEDDING_MODEL, // raw "intfloat/multilingual-e5-large"
       inputs: userQuery,
-      provider: ENV.INFERENCE_PROVIDER,
     });
+    console.log("Embedding result:", embeddingResult);
 
-    return output;
+    return embeddingResult;
   } catch (error) {
     console.error("Error embedding text:", error);
     throw error;
